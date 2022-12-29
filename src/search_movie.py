7a7cd9fe73f96d6f06babbcbaf269ce708cc305f
锟斤拷 <as312d@gmail.com>
2022-12-29 14:44:33 +0800

    Update search_movie.py

--- src/search_movie.py
+++ src/search_movie.py
@@ -2,11 +2,11 @@ import urllib.request
 import requests, re
 from bs4 import BeautifulSoup
 
-def getid(movie_name):
+def getid(movie_name, search_url=None):
     headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",}
     params = {"q": movie_name}
-    # enter your url
-    search_url = "https://www.douban.com/search"
+    if search_url is None:
+        search_url = "https://www.douban.com/search"
     r = requests.get(search_url, params=params, headers=headers)
     soup = BeautifulSoup(r.content, 'lxml')
     first_movie = soup.find('a', {'class': 'nbg'})['onclick']
