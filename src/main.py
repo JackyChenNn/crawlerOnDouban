83a1b5d8f698ef16b4b99aa6574cd23b1e192fc3
锟斤拷 <as312d@gmail.com>
2022-12-29 14:53:25 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -32,6 +32,8 @@ def crawler():
     
 if __name__ == '__main__':
     global name_input, text
+
+    font_type = '微软雅黑'
     
     # 创建空白窗口,作为主载体 
     root = ttk.Window()
@@ -42,21 +44,21 @@ if __name__ == '__main__':
     root.geometry('450x260+200+200')
  
     # 标签控件，窗口中放置文本组件 
-    Label(root, text='请输入电影名:', font=("微软雅黑", 15), fg='black').grid()
+    Label(root, text='请输入电影名:', font=(font_type, 15), fg='black').grid()
  
     # 定位 pack包 place位置 grid是网格式的布局 #Entry是可输入文本框 
-    name_input = Entry(root, font=("微软雅黑", 15))
+    name_input = Entry(root, font=(font_type, 15))
     name_input.grid(row=0, column=1)
  
     # 列表控件 
-    text = Listbox(root, font=('微软雅黑', 15), width=45, height=10)
+    text = Listbox(root, font=(font_type, 15), width=45, height=10)
  
     # columnspan 组件所跨越的列数 
     text.grid(row=1, columnspan=2)
  
     # 设置按钮 sticky对齐方式，N S W E 
-    button = Button(root, text='开始下载', font=("微软雅黑", 15), command=crawler).grid(row=2, column=0, sticky=W)
-    button = Button(root, text='退出', font=("微软雅黑", 15), command=root.quit).grid(row=2, column=1, sticky=E)
+    button1 = Button(root, text='开始下载', font=(font_type, 15), command=crawler).grid(row=2, column=0, sticky=W)
+    button2 = Button(root, text='退出', font=(font_type, 15), command=root.quit).grid(row=2, column=1, sticky=E)
  
     # 使得窗口一直存在 
     mainloop()
